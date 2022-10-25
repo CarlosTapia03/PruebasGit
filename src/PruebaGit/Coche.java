7e4e7524cf83a5386315c04f8036fb7292c652d8
Carlos Tapia <carlosviril3@gmail.com>
2022-10-25 09:40:06 +0200

    setter and getter commit coches

--- src/PruebaGit/Coche.java
+++ src/PruebaGit/Coche.java
@@ -8,6 +8,15 @@ public class Coche {
 		super();
 		this.nombre = nombre;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 	
 	
 	

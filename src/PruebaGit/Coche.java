c7b36ad322862d2bbf400677e75a0b92d4695231
Carlos Tapia <carlosviril3@gmail.com>
2022-10-26 12:17:11 +0200

    commit coches puertas

--- src/PruebaGit/Coche.java
+++ src/PruebaGit/Coche.java
@@ -3,10 +3,12 @@ package PruebaGit;
 public class Coche {
 
 	private String nombre;
+	private int puertas;
 
-	public Coche(String nombre) {
+	public Coche(String nombre, int p) {
 		super();
 		this.nombre = nombre;
+		puertas = p;
 	}
 
 	public String getNombre() {
@@ -16,10 +18,20 @@ public class Coche {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	
+
+	public int getPuertas() {
+		return puertas;
+	}
+
+	public void setPuertas(int puertas) {
+		this.puertas = puertas;
+	}
 
 	@Override
 	public String toString() {
-		return "Coche [nombre=" + nombre + "]";
+		return "Coche [nombre=" + nombre + "puertas= " + puertas + "]";
 	}
 	
 	
